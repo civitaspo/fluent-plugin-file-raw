d7f554296f5c9dce4bca9c6fd372ef7d97ac5a2f
takahiro.nakayama <takahiro.nakayama@dena.com>
2015-06-08 22:39:08 +0900

    write 0.0.1 code.

--- lib/fluent/plugin/out_file_raw.rb
+++ lib/fluent/plugin/out_file_raw.rb
@@ -12,88 +12,115 @@ module Fluent
       super
     end
 
+    config_param :output_path_prefix, :string, :default => '/tmp/fluent_out_file_raw'
+    config_param :bulk_tag_prefix, :string, :default => ''
+    config_param :output_delimiter, :string, :default => ''
+    AVAILABLE_DELIMITERS =
+      {
+        'TAB' => "\t",
+        'COMMA' => ',',
+      }
+
+    # To support log_level option implemented by Fluentd v0.10.43
+    unless method_defined?(:log)
+      define_method("log") { $log }
+    end
+
+    # Define `router` method of v0.12 to support v0.10 or earlier
+    unless method_defined?(:router)
+      define_method("router") { Fluent::Engine }
+    end
+
     def configure(conf)
-      if path = conf['path']
-        @path = path
-      end
-      unless @path
-        fail ConfigError, "'path' parameter is required on file output"
+      super
+
+      unless Dir.exists?(@output_path_prefix)
+        raise Fluent::ConfigError, "out_file_raw: `output_path_prefix` must be existed on this host."
       end
 
-      super
+      unless @bulk_tag_prefix.empty?
+        log.info "out_file_raw: bulk output mode ignores config `output_delimiter`."
+      end
+
+      if @output_delimiter.empty?
+        @delimiter = ''
+      else
+        @delimiter = nil
+        AVAILABLE_DELIMITERS.each do |key, value|
+          if %r{#{key}}miu === @output_delimiter
+            @delimiter = value
+          end
+        end
+
+        unless @delimiter
+          raise Fluent::ConfigError, "out_file_raw: `output_delimiter` must be one of available delimiters [#{AVAILABLE_DELIMITERS.keys.join(',')}]."
+        end
+      end
+
     end
 
     def emit(tag, es, chain)
 
-      date_hour = Time.now.strftime("%Y-%m-%d-%H")
-      hash = rand(32).to_s
-      path = "#{@path}.#{date_hour}.#{hash}"
+      path = "#{@output_path_prefix}.#{date_hour}.#{hash}"
 
-      tag_ary = tag.split('.')
-
-      if tag_ary[0] =~ /^bulk_hlog$/
-        #
-        # bulk_hlog.{JP_XX|KR}.{prod|dev|current}.** or bulk_hlog.ngpipes.{events|events_sandbox}.{jp|kr}**
-        #
-        if tag_ary[1] == 'ngpipes'
-          es.each do | _time, record |
-            if ! record.instance_of?(String)
-              $log.error("record is not String")
-              next
-            end
-            File.open(path, 'a') do |f|
-              f.flock(File::LOCK_EX)
-              StringIO.new(record, 'r').each do | line |
-                f.write(line)
-              end
-            end
+      if bulk?(tag)
+        es.each do |_time, record|
+          unless record.instance_of?(String)
+            log.error("record is not String")
+            next
           end
-        else
-          region = tag_ary[1]
-          env = tag_ary[2]
-
-          es.each do | _time, record |
-            if ! record.instance_of?(String)
-              $log.error("record is not String")
-              next
-            end
-            File.open(path, 'a') do |f|
-              f.flock(File::LOCK_EX)
-              StringIO.new(record, 'r').each do | line |
-                str = "#{region}.#{env}." << line
-                f.write(str)
-              end
+          open_and_lock(path) do |f|
+            StringIO.new(record, 'r').each do |line|
+              f.write(line)
             end
           end
         end
-
       else
-        #
-        # hlog.{JP_XX|KR}.** or ngpipes.**
-        #
-        if tag_ary[0] != 'ngpipes'
-          region = tag_ary[1]
-          env = tag_ary[2]
-        end
-
-        File.open(path, 'a') do |f|
-          f.flock(File::LOCK_EX)
-          es.each do | _time, record |
-            if ! record.instance_of?(Array)
-              $log.error("record is not Array")
+        open_and_lock(path) do |f|
+          es.each do |_time, record|
+            unless record.instance_of?(Array)
+              log.error("record is not Array")
               next
             end
-            if tag_ary[0] == 'ngpipes'
-              str = record.join("\t") << "\n"
-            else
-              str = "#{region}.#{env}." << record.join("\t") << "\n"
-            end
-            f.write(str)
+            f.write(record_formatter(record))
           end
         end
       end
 
       chain.next
+    end
+
+    private
+
+    def date_hour
+      Time.now.strftime("%Y-%m-%d-%H")
+    end
+
+    def hash
+      rand(32).to_s
+    end
+
+    def bulk?(tag)
+      if @bulk_tag_prefix
+        %r{^#{@bulk_tag_prefix}$} === tag.split('.')[0]
+      else
+        false
+      end
+    end
+
+    def record_formatter(record)
+      record.join(@delimiter) << "\n"
+    end
+
+    def open_and_lock(path)
+      if block_given?
+        File.open(path, 'a') do |f|
+          f.flock(File::LOCK_EX)
+          yield(f)
+        end
+      else
+        raise RuntimeError, "out_file_raw: 'open_and_lock' method is available on block given context."
+      end
     end
   end
 end
